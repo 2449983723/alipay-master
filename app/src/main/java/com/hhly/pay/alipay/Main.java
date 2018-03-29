ab0d51bc5050f906db4c430e1c5101ada59f378e
liunian <liun759@13322.com>
2018-03-29 14:58:37 +0800

    提交支付宝插件

--- app/src/main/java/com/hhly/pay/alipay/Main.java
+++ app/src/main/java/com/hhly/pay/alipay/Main.java
@@ -1,10 +1,15 @@
 package com.hhly.pay.alipay;
 
 import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 import static com.hhly.pay.alipay.VersionParam.ALIPAY_PACKAGE_NAME;
@@ -17,8 +22,32 @@ public class Main implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        if (lpparam.packageName.equals(ALIPAY_PACKAGE_NAME)) {
+        if (lpparam.appInfo == null || (lpparam.appInfo.flags & (ApplicationInfo.FLAG_SYSTEM |
+                ApplicationInfo.FLAG_UPDATED_SYSTEM_APP)) != 0) {
+            return;
+        }
+        final String packageName = lpparam.packageName;
+
+        if (packageName.equals(BuildConfig.APPLICATION_ID)) {
+            XposedHelpers.findAndHookMethod(BuildConfig.APPLICATION_ID + ".MainActivity", lpparam.classLoader,
+                    "isModuleActive", XC_MethodReplacement.returnConstant(true));
+            return;
+        }
+
+        if (packageName.equals(ALIPAY_PACKAGE_NAME)) {
             // hook 个人收取的onActivityResult方法
+            XposedHelpers.findAndHookMethod(Application.class,
+                    "attach",
+                    Context.class, new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                            super.afterHookedMethod(param);
+                            Context context = (Context) param.args[0];
+                            ClassLoader appClassLoader = context.getClassLoader();
+                            securityCheckHook(appClassLoader);
+                        }
+                    });
+
             findAndHookMethod("com.alipay.mobile.payee.ui.PayeeQRActivity", lpparam.classLoader, "onActivityResult", int.class, int.class, Intent.class, new XC_MethodHook() {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
@@ -35,6 +64,42 @@ public class Main implements IXposedHookLoadPackage {
                 }
             });
         }
+    }
 
+    private void securityCheckHook(ClassLoader classLoader) {
+        try {
+            Class securityCheckClazz = XposedHelpers.findClass("com.alipay.mobile.base.security.CI", classLoader);
+            XposedHelpers.findAndHookMethod(securityCheckClazz, "a", String.class, String.class, String.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    Object object = param.getResult();
+                    XposedHelpers.setBooleanField(object, "a", false);
+                    param.setResult(object);
+                    super.afterHookedMethod(param);
+                }
+            });
+
+            XposedHelpers.findAndHookMethod(securityCheckClazz, "a", Class.class, String.class, String.class, new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                    return (byte) 1;
+                }
+            });
+            XposedHelpers.findAndHookMethod(securityCheckClazz, "a", ClassLoader.class, String.class, new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                    return (byte) 1;
+                }
+            });
+            XposedHelpers.findAndHookMethod(securityCheckClazz, "a", new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                    return false;
+                }
+            });
+
+        } catch (Error | Exception e) {
+            e.printStackTrace();
+        }
     }
 }
